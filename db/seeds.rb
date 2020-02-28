a45871057f88c00ff3702778835142ee6832f734
Justine Barbauk <justinehsd@gmail.com>
2020-02-28 11:44:26 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 require 'json'
 require 'open-uri'
 
-Ingredient.destroy_all
 Cocktail.destroy_all
+Ingredient.destroy_all
 Dose.destroy_all
 
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
